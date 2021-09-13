e6fcda31bc599dc5bb3a3828456ae9f4f8f02e02
Hamz Einfach <85156241+Hamz-einfach@users.noreply.github.com>
2021-09-13 16:11:00 +0700

    Create testunitmove.cpp

---
+++ review/testunitmove.cpp
@@ -0,0 +1,10 @@
+class CfgUnitInsignia
+{
+ class myInsigniaID
+ {
+  displayName = "Some ArmA Group"; // Name displayed in Arsenal
+  author = "Some Guy"; // Author displayed in Arsenal
+  texture = "myCustomInsignia.paa"; // Image path
+  textureVehicle = ""; // Does nothing currently, reserved for future use
+ };
+};
